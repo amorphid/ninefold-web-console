6796140534a2be8b7bfb186cf3e80080ac23930c
Michael Pope <amorphid@captaincoder.io>
2013-11-01 21:18:20 -0700

    Add enviroment variable 'WHITELIST_IP' to whitelisted IPs for web-console

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module NinefoldWebConsole
   class Application < Rails::Application
-    config.web_console.whitelisted_ips = %w( 127.0.0.1 )
+    config.web_console.whitelisted_ips = [
+                                            "127.0.0.1",
+                                            ENV["WHITELIST_IP"]
+                                         ]
   end
 end
